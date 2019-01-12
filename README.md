64d54756638709065930a6a4af396aad5b6d3b96
Ang Lee <ang.lee@gmail.com>
2019-01-12 16:29:31 -0500

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+Code example from [Martin Fowler](https://martinfowler.com/)'s book - [Refactoring v2](https://martinfowler.com/books/refactoring.html).
+
+[Chapter 1 — Refactoring: A First Example](https://memberservices.informit.com/my_account/webedition/9780135425664/html/opening.html)
---
+++ src/statement.test.js
@@ -0,0 +1,9 @@
+const statement = require("./statement");
+const invoice = require("./data/invoice");
+const plays = require("./data/plays");
+
+describe("statement", () => {
+  it("prints statement with given invoice and plays", () => {
+    expect(statement(invoice[0], plays)).toMatchSnapshot();
+  });
+});
