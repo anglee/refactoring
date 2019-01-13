68c353b53c073b7bb5254b3a2b58563c75174d92
Ang Lee <ang.lee@gmail.com>
2019-01-13 10:12:11 -0500

    Make format a local function

--- src/statement.js
+++ src/statement.js
@@ -2,11 +2,6 @@ function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2
-  }).format;
 
   for (let perf of invoice.performances) {
     // add volume credits
@@ -58,6 +53,13 @@ function statement(invoice, plays) {
     }
 
     return result;
+  }
+  function format(aNumber) {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 2
+    }).format(aNumber);
   }
 }
 
