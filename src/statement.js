f67ca5e30927614b2fa67300de4f27729c348699
Ang Lee <ang.lee@gmail.com>
2019-01-13 10:22:28 -0500

    Extract totalVolumeCredits

--- src/statement.js
+++ src/statement.js
@@ -2,7 +2,6 @@ function statement(invoice, plays) {
   let totalAmount = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
-
   for (let perf of invoice.performances) {
     // print line for this order
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${
@@ -11,12 +10,7 @@ function statement(invoice, plays) {
     totalAmount += amountFor(perf);
   }
 
-  let volumeCredits = 0;
-  for (let perf of invoice.performances) {
-    // add volume credits
-    volumeCredits += volumeCreditsFor(perf);
-  }
-
+  let volumeCredits = totalVolumeCredits();
   result += `Amount owed is ${usd(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
@@ -58,12 +52,22 @@ function statement(invoice, plays) {
 
     return result;
   }
+
   function usd(aNumber) {
     return new Intl.NumberFormat("en-US", {
       style: "currency",
       currency: "USD",
       minimumFractionDigits: 2
     }).format(aNumber);
+  }
+
+  function totalVolumeCredits() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      // add volume credits
+      result += volumeCreditsFor(perf);
+    }
+    return result;
   }
 }
 
