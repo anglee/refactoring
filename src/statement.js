f979a59f721cbca5cb36c30611d307987e5294d0
Ang Lee <ang.lee@gmail.com>
2019-01-14 08:23:33 -0500

    09, slide statements for volumeCredit calculation

--- src/statement.js
+++ src/statement.js
@@ -1,11 +1,7 @@
 function statement(invoice, plays) {
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
-  for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-  }
   for (let perf of invoice.performances) {
     // print line for this order
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${
@@ -14,8 +10,16 @@ function statement(invoice, plays) {
     totalAmount += amountFor(perf);
   }
   result += `Amount owed is ${usd(totalAmount / 100)}\n`;
+
+  let volumeCredits = 0;
+  for (let perf of invoice.performances) {
+    volumeCredits += volumeCreditsFor(perf);
+  }
   result += `You earned ${volumeCredits} credits\n`;
+
   return result;
+
+  //==========================================================================
 
   function amountFor(aPerformance) {
     let result = 0;
