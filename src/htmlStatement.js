08a7983a0a5851a8e17735b9d1829f2289d87383
Ang Lee <ang.lee@gmail.com>
2019-01-15 09:33:37 -0500

    20, add htmlStatement() and tests

---
+++ src/htmlStatement.js
@@ -0,0 +1,23 @@
+const createStatementData = require("./createStatementData");
+const { usd } = require("./utils");
+
+function htmlStatement(invoice, plays) {
+  const statementData = createStatementData(invoice, plays);
+  return renderHtml(statementData);
+}
+
+function renderHtml (data) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+  result += "<table>\n";
+  result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
+  for (let perf of data.performances) {
+    result += `  <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
+  }
+  result += "</table>\n";
+  result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+  return result;
+}
+
+module.exports = htmlStatement;
--- src/statement.test.js
+++ src/statement.test.js
@@ -3,7 +3,7 @@ const invoice = require("./data/invoice");
 const plays = require("./data/plays");
 
 describe("statement", () => {
-  it("prints statement with given invoice and plays", () => {
+  it("prints plain text statement with given invoice and plays", () => {
     expect(statement(invoice[0], plays)).toMatchSnapshot();
   });
 });
