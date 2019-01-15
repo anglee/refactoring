ad9e457c4a18d029a1206bc8dab8ac8dd5c24466
Ang Lee <ang.lee@gmail.com>
2019-01-15 06:59:03 -0500

    08, rename format() to usd()

--- src/statement.js
+++ src/statement.js
@@ -7,12 +7,12 @@ function statement(invoice, plays) {
     volumeCredits += volumeCreditsFor(perf);
 
     // print line for this order
-    result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+    result += `  ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${
       perf.audience
     } seats)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `Amount owed is ${usd(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 
@@ -60,7 +60,7 @@ function statement(invoice, plays) {
     return volumeCredits;
   }
 
-  function format(aNumber) {
+  function usd(aNumber) {
     return new Intl.NumberFormat("en-US", {
       style: "currency",
       currency: "USD",
