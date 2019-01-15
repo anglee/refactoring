259b9a1f840c66bcc53b84f0ebf5e425f77d0227
Ang Lee <ang.lee@gmail.com>
2019-01-15 09:23:30 -0500

    18, extract createStatementData() and move it into its own file

---
+++ src/createStatementData.js
@@ -0,0 +1,69 @@
+function createStatementData(invoice, plays) {
+  const performances = invoice.performances.map(performance => {
+    const perf = { ...performance };
+    perf.play = playFor(perf);
+    perf.amount = amountFor(perf);
+    perf.volumeCredits = volumeCreditsFor(perf);
+    return perf;
+  });
+  const statementData = {
+    customer: invoice.customer,
+    performances,
+    totalAmount: totalAmount(performances),
+    totalVolumeCredits: totalVolumeCredits(performances)
+  };
+  return statementData;
+
+  //============================================================================
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+
+  function amountFor(aPerformance) {
+    let result = 0;
+
+    switch (aPerformance.play.type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${aPerformance.play.type}`);
+    }
+
+    return result;
+  }
+
+  function volumeCreditsFor(perf) {
+    let volumeCredits = 0;
+
+    // add volume credits
+    volumeCredits += Math.max(perf.audience - 30, 0);
+
+    // add extra credit for every ten comedy attendees
+    if ("comedy" === perf.play.type) {
+      volumeCredits += Math.floor(perf.audience / 5);
+    }
+
+    return volumeCredits;
+  }
+
+  function totalAmount(performances) {
+    return performances.reduce((total, p) => total + p.amount, 0);
+  }
+
+  function totalVolumeCredits(performances) {
+    return performances.reduce((total, p) => total + p.volumeCredits, 0);
+  }
+}
+
+module.exports = createStatementData;
--- src/statement.js
+++ src/statement.js
@@ -1,69 +1,8 @@
+const createStatementData = require("./createStatementData");
+
 function statement(invoice, plays) {
-  const statementData = {};
-  statementData.customer = invoice.customer;
-  const performances = invoice.performances.map(performance => {
-    const perf = { ...performance };
-    perf.play = playFor(perf);
-    perf.amount = amountFor(perf);
-    perf.volumeCredits = volumeCreditsFor(perf);
-    return perf;
-  });
-  statementData.performances = performances;
-  statementData.totalAmount = totalAmount(performances);
-  statementData.totalVolumeCredits = totalVolumeCredits(performances);
-
+  const statementData = createStatementData(invoice, plays);
   return renderPlainText(statementData);
-
-  //============================================================================
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
-
-  function amountFor(aPerformance) {
-    let result = 0;
-
-    switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${aPerformance.play.type}`);
-    }
-
-    return result;
-  }
-
-  function volumeCreditsFor(perf) {
-    let volumeCredits = 0;
-
-    // add volume credits
-    volumeCredits += Math.max(perf.audience - 30, 0);
-
-    // add extra credit for every ten comedy attendees
-    if ("comedy" === perf.play.type) {
-      volumeCredits += Math.floor(perf.audience / 5);
-    }
-
-    return volumeCredits;
-  }
-
-  function totalAmount(performances) {
-    return performances.reduce((total, p) => total + p.amount, 0);
-  }
-
-  function totalVolumeCredits(performances) {
-    return performances.reduce((total, p) => total + p.volumeCredits, 0);
-  }
 }
 
 function renderPlainText(data) {
