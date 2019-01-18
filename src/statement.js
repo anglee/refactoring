6d9f3616670f23af008d67de64d1ee2864573118
Ang Lee <ang.lee@gmail.com>
2019-01-18 07:27:41 -0500

    06, split the for-loop

--- src/statement.js
+++ src/statement.js
@@ -6,17 +6,20 @@ function statement(invoice, plays) {
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
-
     volumeCredits += volumeCreditsFor(perf);
+  }
 
+  for (let perf of invoice.performances) {
     // print line for this order
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${
       perf.audience
-    } seats)\n`;
-
-    totalAmount += amountFor(perf);
-
+      } seats)\n`;
   }
+
+  for (let perf of invoice.performances) {
+    totalAmount += amountFor(perf);
+  }
+
   result += `Amount owed is ${usd(totalAmount / 100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
